65208cddbca099df18f99f732fdc5add150d4b63
田中颯太 <TanakaSouta@tanakafuuta-no-macbook-pro.local>
2016-06-01 19:07:31 +0900

    Initial Commit

---
+++ Lit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lit
+//
+//  Created by 田中颯太 on 2016/06/01.
+//  Copyright © 2016年 田中颯太. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
