d4c792d13c035963925009d77838cba2b2a4de98
田中颯太 <tanakakesouta@yahoo.co.jp>
2016-06-01 20:40:24 +0900

    webview

--- WebViewController.swift
+++ WebViewController.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2016年 田中颯太. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController ,UIWebViewDelegate{
+    
+    @IBOutlet weak var webview: UIWebView!
+    var targetURL = "https://www.life-is-tech.com"
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webview.scalesPageToFit = true
+         loadAddressURL()
+        // Do any additional setup after loading the view, typically from a nib.
+            }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func loadAddressURL() {
+        let requestURL = NSURL(string: targetURL)
+        let req = NSURLRequest(URL: requestURL!)
+        webview.loadRequest(req)
+    }
+    //ステータスバーを非表示
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
 
 }
