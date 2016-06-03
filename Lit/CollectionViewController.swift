77e93fa4fde049f336e676425c6f929c2f560623
田中颯太 <tanakakesouta@yahoo.co.jp>
2016-06-03 23:35:07 +0900

    コレクションビュー

---
+++ Lit/CollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//  Lit
+//
+//  Created by 田中颯太 on 2016/06/03.
+//  Copyright © 2016年 田中颯太. All rights reserved.
+//
+
+import Cocoa
+
+class CollectionViewController: UICollectionViewController {
+
+}
