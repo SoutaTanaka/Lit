59275042214f5529ae356a08b018a76f76a2425f
田中颯太 <TanakaSouta@tanakafuuta-no-MacBook-Pro.local>
2016-06-22 20:33:09 +0900

    collectionView

--- Lit/SecondViewController.swift
+++ Lit/SecondViewController.swift
@@ -4,17 +4,55 @@
 //
 //  Created by 田中颯太 on 2016/06/01.
 //  Copyright © 2016年 田中颯太. All rights reserved.
-//
+//参考　http://anthrgrnwrld.hatenablog.com/entry/2015/10/27/221856
+//　https://akira-watson.com/iphone/collectionview.html
 
 import UIKit
 import AssetsLibrary
-class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    let photo = ["testing.jpg","tess.png","m1.png","m2.png","m3.jpeg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        
+        // dequeueReusableCellWithReuseIdentifier の働きは
+        // 再利用できるセルがあればそれを使う
+        // 再利用できるセルがなければ生成する
+        // Cell はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+        
+        // Tag番号を使ってImageViewのインスタンス生成
+        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
+        let cellImage = UIImage(named: photo[indexPath.row])
+        // UIImageをUIImageViewのimageとして設定
+        imageView.image = cellImage
+        
+        
+        return testCell
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let cellSize:CGFloat = self.view.frame.size.width/2 - 10
+        return CGSizeMake(cellSize, cellSize)
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5;
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
