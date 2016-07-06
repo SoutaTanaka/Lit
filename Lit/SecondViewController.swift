e0facb39223ed21725acf78dfdc559de0d6c9c6c
田中颯太 <TanakaSouta@tanakafuuta-no-MacBook-Pro.local>
2016-07-06 20:37:20 +0900

    コレクションviewへの保存

--- Lit/SecondViewController.swift
+++ Lit/SecondViewController.swift
@@ -4,40 +4,33 @@
 //
 //  Created by 田中颯太 on 2016/06/01.
 //  Copyright © 2016年 田中颯太. All rights reserved.
-//参考　http://anthrgrnwrld.hatenablog.com/entry/2015/10/27/221856
+//　http://anthrgrnwrld.hatenablog.com/entry/2015/10/27/221856
 //　https://akira-watson.com/iphone/collectionview.html
 
 import UIKit
 import AssetsLibrary
 class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    let photo = ["testing.jpg","tess.png","m1.png","m2.png","m3.jpeg"]
-
+    var photo:[NSData] = []
+    @IBOutlet var collection: UICollectionView!
+    let data: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+    
+    
     override func viewDidLoad() {
+        if data.arrayForKey("photos") != nil{
+            photo = data.arrayForKey("photos") as! [NSData]
+        }
+        
         super.viewDidLoad()
-       
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
-        
-        // dequeueReusableCellWithReuseIdentifier の働きは
-        // 再利用できるセルがあればそれを使う
-        // 再利用できるセルがなければ生成する
-        // Cell はストーリーボードで設定したセルのID
         let testCell:UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
-        
-        // Tag番号を使ってImageViewのインスタンス生成
         let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
-        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
-        let cellImage = UIImage(named: photo[indexPath.row])
-        // UIImageをUIImageViewのimageとして設定
-        imageView.image = cellImage
-        
-        
+        let cellImage = photo[indexPath.row]
+        imageView.image = UIImage(data: cellImage)
         return testCell
     }
-    
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let cellSize:CGFloat = self.view.frame.size.width/2 - 10
@@ -49,25 +42,38 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5;
+        return photo.count
         
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func pictur(sender: AnyObject) {
         if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary)){
             let imagePickerController = UIImagePickerController()
             imagePickerController.sourceType = .PhotoLibrary
             imagePickerController.allowsEditing = true
-//            imagePickerController.delegate = self
+            imagePickerController.delegate = self
             presentViewController(imagePickerController, animated: true, completion: nil)
         }
     }
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if  let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+            let nsdata: NSData = UIImagePNGRepresentation(editedImage)!
+            photo.append (nsdata)
+            collection.reloadData()
+            data.setObject(photo, forKey: "photos")
+        }
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+        
 }
+
 
